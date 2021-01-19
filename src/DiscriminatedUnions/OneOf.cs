08b460045c8cab3b6f335bef6cf26fda8a8193b5
batiati <rbatiati@gmail.com>
2021-01-19 10:52:27 -0300

    first commit

---
+++ src/DiscriminatedUnions/OneOf.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace System
+{
+	public struct OneOf<T1, T2> : IEquatable<OneOf<T1, T2>>
+	{
+		#region Fields
+
+		private readonly object value;
+
+		#endregion Fields
+
+		#region Properties
+
+		public object Value => value;
+
+		#endregion Properties
+
+		#region Constructors
+		public OneOf(T1 value)
+		{
+			this.value = value;
+		}
+
+		public OneOf(T2 value)
+		{
+			this.value = value;
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		public bool TryGet(out T1 value)
+		{
+			bool success;
+			(value, success) = this.value is T1 _value ? (_value, true) : (default(T1), false);
+
+			return success;
+		}
+
+		public bool TryGet(out T2 value)
+		{
+			bool success;
+			(value, success) = this.value is T2 _value ? (_value, true) : (default(T2), false);
+
+			return success;
+		}
+
+		public bool Equals(OneOf<T1, T2> other)
+		{
+			return object.Equals(value, other.value);
+		}
+
+		public bool Equals(T1 other)
+		{
+			return object.Equals(value, other);
+		}
+
+		public bool Equals(T2 other)
+		{
+			return object.Equals(value, other);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return object.Equals(value, obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return value?.GetHashCode() ?? 0;
+		}
+
+		public static implicit operator T1(OneOf<T1, T2> union)
+		{
+			return (T1)union.value;
+		}
+
+		public static implicit operator OneOf<T1, T2>(T1 value)
+		{
+			return new OneOf<T1, T2>(value);
+		}
+
+		public static implicit operator T2(OneOf<T1, T2> union)
+		{
+			return (T2)union.value;
+		}
+
+		public static implicit operator OneOf<T1, T2>(T2 value)
+		{
+			return new OneOf<T1, T2>(value);
+		}
+
+		public static bool operator ==(OneOf<T1, T2> a, OneOf<T1, T2> b)
+		{
+			return object.Equals(a.value, b.value);
+		}
+
+		public static bool operator !=(OneOf<T1, T2> a, OneOf<T1, T2> b)
+		{
+			return !object.Equals(a.value, b.value);
+		}
+
+		#endregion Methods
+	}
+}
---
+++ src/Tests/Test.cs
@@ -0,0 +1,162 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+	public class Tests
+	{
+		[Test]
+		public void IsValueTypeTest()
+		{
+			var value = new OneOf<int, string>(100);
+			Assert.IsTrue(value.TryGet(out int i32));
+			Assert.AreEqual(i32, 100);
+		}
+
+		[Test]
+		public void IsNotValueTypeTest()
+		{
+			var value = new OneOf<int, long>(1000);
+			Assert.IsFalse(value.TryGet(out long i64));
+			Assert.AreNotEqual(i64, 1000);
+		}
+
+		[Test]
+		public void ValueTypeEqualsTest()
+		{
+			var value1 = new OneOf<int, string>(100);
+			var value2 = new OneOf<int, string>(100);
+
+			Assert.AreEqual(value1, value2);
+			Assert.AreEqual(value1, 100);
+			Assert.AreEqual(value2, 100);
+		}
+
+		[Test]
+		public void ValueTypeCastTest()
+		{
+			int i32 = 100;
+			OneOf<int, string> value1 = i32;
+			int i32_b = value1;
+
+			Assert.AreEqual(value1, i32);
+			Assert.AreEqual(value1, i32_b);
+			Assert.AreEqual(i32, i32_b);
+		}
+
+		[Test]
+		public void IsRefTypeTest()
+		{
+			var value = new OneOf<int, string>("hello");
+			Assert.IsTrue(value.TryGet(out string hello));
+			Assert.AreEqual(hello, "hello");
+		}
+
+		[Test]
+		public void IsNotRefTypeTest()
+		{
+			var value = new OneOf<Exception, string>(new Exception("hello"));
+			Assert.IsFalse(value.TryGet(out string hello));
+			Assert.AreNotEqual(hello, "hello");
+		}
+
+		[Test]
+		public void RefTypeEqualsTest()
+		{
+			var value1 = new OneOf<string, Exception>("hello");
+			var value2 = new OneOf<string, Exception>("hello");
+
+			Assert.AreEqual(value1, value2);
+			Assert.AreEqual(value1, "hello");
+			Assert.AreEqual(value2, "hello");
+		}
+
+		[Test]
+		public void RefTypeCastTest()
+		{
+			string str = "hello";
+			OneOf<int, string> value1 = str;
+			string str_b = value1;
+
+			Assert.AreEqual(value1, str);
+			Assert.AreEqual(value1, str_b);
+			Assert.AreEqual(str, str_b);
+		}
+
+		[Test]
+		public void IsBothByInheritanceTest()
+		{
+			var value = new OneOf<object, string>("hello");
+			
+			Assert.IsTrue(value.TryGet(out string hello));
+			Assert.AreEqual(hello, "hello");
+
+			Assert.IsTrue(value.TryGet(out object obj));
+			Assert.AreEqual(obj, "hello");
+		}
+
+		[Test]
+		public void ReturnValueTest()
+		{
+			OneOf<int, string> divideBy(int a, int b)
+			{
+				if (b == 0) return "Cannot divide by zero";
+				return a / b;
+			}
+
+			var ret1 = divideBy(10, 0);
+			Assert.IsTrue(ret1.TryGet(out string _));
+			Assert.AreEqual(ret1, "Cannot divide by zero");
+
+			var ret2 = divideBy(10, 2);
+			Assert.IsTrue(ret2.TryGet(out int _));
+			Assert.AreEqual(ret2, 5);
+		}
+
+		[Test]
+		public void ArgumentTest()
+		{
+			OneOf<int, string> sum(OneOf<int, string> a, OneOf<int, string> b)
+			{
+				if (a.TryGet(out int aI32))
+				{
+					if (b.TryGet(out int bI32))
+					{
+						return aI32 + bI32;
+					}
+					else if (b.TryGet(out string bStr))
+					{
+						return string.Concat(aI32.ToString(), bStr);
+					}
+				}
+				else if (a.TryGet(out string aStr))
+				{
+					if (b.TryGet(out int bI32))
+					{
+						return string.Concat(aStr, bI32.ToString());
+					}
+					else if (b.TryGet(out string bStr))
+					{
+						return string.Concat(aStr, bStr);
+					}
+				}
+
+				throw new NotImplementedException();
+			}
+
+			var ret1 = sum(10, 10);
+			Assert.AreEqual(ret1, 20);
+
+			var ret2 = sum("10", 10);
+			Assert.AreEqual(ret2, "1010");
+
+			var ret3 = sum(10, "10");
+			Assert.AreEqual(ret3, "1010");
+
+			var ret4 = sum("10", "10");
+			Assert.AreEqual(ret4, "1010");
+		}
+
+
+	}
+}
